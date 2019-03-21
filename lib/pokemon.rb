89b821f66606bb502769f7cb1e20dce6b54c60e9
Joan Lyness <joanrigdon@gmail.com>
2019-03-21 02:16:44 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -11,8 +11,16 @@ class Pokemon
     @@all
   end
 
+<<<<<<< HEAD
   def self.save(name, type, db)
     db.execute("INSERT INTO Pokemon (name, type) VALUES (?, ?)", name, type)
+=======
+  def save(name, type, id)
+    @name = name
+    @type = type
+    @id = id
+    @@all<< self
+>>>>>>> e8de547b8836f94d282381fa4ffc1fb6ec18b444
   end
 
   def self.find(id)
