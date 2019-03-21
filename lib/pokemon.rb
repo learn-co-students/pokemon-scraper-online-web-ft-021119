cab30bf6ca2bb9aa1deaf1cd7767a29e235bff45
Joan Lyness <joanrigdon@gmail.com>
2019-03-21 02:30:06 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,9 +1,9 @@
 class Pokemon
-  attr_accessor :id, :name, :type, :db
+  attr_accessor :id, :hp, :name, :type, :db
 
   @@all = []
 
-  def initialize(data)
+  def initialize(data, hp=nil)
   end
 
   def self.all
@@ -21,7 +21,12 @@ class Pokemon
     new_pokeman.id = data[0][0]
     new_pokeman.name = data[0][1]
     new_pokeman.type = data[0][2]
+    new_pokeman.hp = data[0][3]
     new_pokeman
+  end
+
+  def BONUS
+    db.execute("INSERT INTO Pokemon (hp) VALUES (?)", 60)
   end
 
 end
