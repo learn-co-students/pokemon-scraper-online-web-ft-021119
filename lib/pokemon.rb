af2bfe70af0fff7d51b2e44178b7c94ed6a37b47
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-22 18:35:11 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,20 @@
+
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  @@all = []
+
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    @@all << self
+  end
+
+  def self.save(name,type,db)
+    db.execute("INSERT INTO Pokemon(name,type) VALUES (?,?)",name,type)
+  end
+  # In the database, pass in the scraped name and type. Execute (on the db)...insert these into Pokemon table.
+  # And the values are from the arguments passed in
 end
